b8de5c2628f7d1263accdefa005e4f4774ce2262
David-Parkerr <davidparker0040@gmail.com>
2025-10-11 17:13:40 -0400

    Add simple dialgue for character intros

--- Globals/Global.gd
+++ Globals/Global.gd
@@ -1,1 +1,3 @@
 extends Node
+
+var randomizer: Randomizer = Randomizer.new()
---
+++ Helpers/Randomizer.gd
@@ -0,0 +1,16 @@
+class_name Randomizer extends Node
+
+var rng = RandomNumberGenerator.new()
+
+var roll_result: bool = true
+
+# Provide number between 0 and 1
+func roll(percentage: float) -> void:
+	if (percentage < 0 or percentage > 1):
+		self.roll_result = false
+		
+	var num = rng.randf()
+	if num > percentage:
+		self.roll_result = false
+	else:
+		self.roll_result = true
