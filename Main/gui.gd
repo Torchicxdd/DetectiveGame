8fdaf650843514c1ff5aff73d2482f83bf2a7b54
David-Parkerr <davidparker0040@gmail.com>
2025-10-14 06:59:52 -0400

    Add example of deleting journal with gui functions

--- Main/gui.gd
+++ Main/gui.gd
@@ -4,11 +4,7 @@ var journal = preload("res://Scenes/UI/Journal/Journal.tscn")
 
 func _ready() -> void:
 	SignalBus.connect("open_journal", Callable(self, "_open_journal"))
-	SignalBus.connect("close_journal", Callable(self, "_close_journal"))
 
 func _open_journal() -> void:
 	var instanced_journal = journal.instantiate()
 	Global.main.addGUIScene(instanced_journal)
-
-func _close_journal() -> void:
-	Global.main.deleteGUIScene(journal)
--- Scenes/UI/Journal/journal.gd
+++ Scenes/UI/Journal/journal.gd
@@ -1,1 +1,10 @@
 class_name Journal extends Control
+
+func _ready() -> void:
+	SignalBus.connect("close_journal", Callable(self, "_close_journal"))
+
+func _close_journal() -> void:
+	Global.main.deleteGUIScene(self)
+
+func _on_mouse_entered() -> void:
+	SignalBus.emit_signal("close_journal")
