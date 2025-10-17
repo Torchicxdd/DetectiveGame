a507ce8c709882c4d7b9eb31e219eba849fa1eb3
David-Parkerr <davidparker0040@gmail.com>
2025-10-17 15:41:57 -0400

    Add inner thoughts and normal dialogue and connect to viewer

--- Scenes/UI/Dialogue/dialogue_item.gd
+++ Scenes/UI/Dialogue/dialogue_item.gd
@@ -1,7 +1,10 @@
 class_name DialogueItem extends VBoxContainer
 
-func set_character_name(name: String) -> void:
-	$Character.text = "~ " + name
+func set_character_name(name: String, is_inner_thoughts: bool = false) -> void:
+	if (is_inner_thoughts):
+		$Character.text = "~ " + name
+	else:
+		$Character.text = name
 
 func set_dialogue(dialogue: String) -> void:
 	$Dialogue.text = dialogue
--- Scenes/UI/DialogueViewer/dialogue_viewer.gd
+++ Scenes/UI/DialogueViewer/dialogue_viewer.gd
@@ -1,16 +1,20 @@
 class_name DialogueViewer extends Control
 
-var dialogue_item_scene = preload("res://Scenes/UI/Dialogue/DialogueItem.tscn")
+var normal_dialogue_scene = preload("res://Scenes/UI/Dialogue/NormalDialogue.tscn")
+var inner_thoughts_scene = preload("res://Scenes/UI/Dialogue/InnerThoughtsDialogue.tscn")
 
 func _ready() -> void:
-	for i in range(10):
-		var instance = dialogue_item_scene.instantiate() as DialogueItem
-		instance.set_character_name("Inner Thoughts")
-		if (i != 2):
-			instance.set_dialogue("I- I was walking my son to get some candy for Halloween. Inner Thoughts: He’s not thinking about the question you just asked him. He’s thinking about the wife back home cooking up a storm fully knowing the he was supposed to be back home by 9:30.")
-		else:
-			instance.set_dialogue("Example")
-		$Wrapper/ScrollContainer/MarginContainer/VDialogueDisplay.add_child(instance)
+	SignalBus.connect("add_dialogue", Callable(self, "_add_dialogue"))
+
+func _add_dialogue(name: String, dialogue: String, is_inner_thoughts: bool) -> void:
+	var instance: DialogueItem
+	if (is_inner_thoughts):
+		instance = inner_thoughts_scene.instantiate()
+	else:
+		instance = normal_dialogue_scene.instantiate()
 		
+	instance.set_character_name(name, true)
+	instance.set_dialogue(dialogue)
+	$Wrapper/ScrollContainer/MarginContainer/VDialogueDisplay.add_child(instance)
 	# Have to redraw in order for separation to take effect for vbox
 	$Wrapper/ScrollContainer/MarginContainer/VDialogueDisplay.queue_redraw()
