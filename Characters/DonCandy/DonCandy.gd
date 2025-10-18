188569ca7ba6ad4076ea0c96835f4a6abb4ab3ba
David-Parkerr <davidparker0040@gmail.com>
2025-10-18 02:17:11 -0400

    Fix don candy bug

--- Characters/DonCandy/DonCandy.gd
+++ Characters/DonCandy/DonCandy.gd
@@ -3,3 +3,8 @@ class_name DonCandy extends Character
 func _init() -> void:
 	character_name = "Little Child"
 	silent_treatment_percentage = 50
+	age = 7
+	height = 122
+	gender = "Male"
+	hair_color = "Red"
+	headshot = preload("res://Characters/DonCandy/LittleChildHeadshot.png")
